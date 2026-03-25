f774cbaeebf02874cd6da6bbe409ed2f3b04841e
= <kaiqueklock@gmail.com>
2026-03-25 17:31:19 -0300

    Release 1.0.0+4

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+
 class _MyAppState extends State<MyApp> {
   late Box settingsBox;
   late ThemeMode _themeMode;
